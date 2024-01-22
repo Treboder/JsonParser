efc029021ec48fc32ee313097894c0ac25c5aa04
KRO5RT <robert.kohn@bosch.com>
2024-01-22 10:35:49 +0100

    Read file

--- src/main/java/com/example/JsonParser/JsonParserApplication.java
+++ src/main/java/com/example/JsonParser/JsonParserApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.*;
+
 @SpringBootApplication
 public class JsonParserApplication implements CommandLineRunner {
 
@@ -18,7 +20,27 @@ public class JsonParserApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		logger.info("STARTING JsonParserApplication via command line runner");
+		readFile();
 	}
 
+	private void readFile() throws IOException {
+		ClassLoader classLoader = getClass().getClassLoader();
+		String fileName = "./data/a1.json";
+		InputStream inputStream = new FileInputStream(fileName);
+		String data = readFromInputStream(inputStream);
+		logger.info("Read file ",fileName);
+	}
+
+	private String readFromInputStream(InputStream inputStream) throws IOException {
+		StringBuilder resultStringBuilder = new StringBuilder();
+		try (BufferedReader br
+					 = new BufferedReader(new InputStreamReader(inputStream))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				resultStringBuilder.append(line).append("\n");
+			}
+		}
+		return resultStringBuilder.toString();
+	}
 
 }
