b468ef05c995c482cb1e2df2ddb0e85735e50295
KRO5RT <robert.kohn@bosch.com>
2024-01-22 10:23:42 +0100

    Start with command line runner

--- src/main/java/com/example/JsonParser/JsonParserApplication.java
+++ src/main/java/com/example/JsonParser/JsonParserApplication.java
@@ -1,13 +1,24 @@
 package com.example.JsonParser;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JsonParserApplication {
+public class JsonParserApplication implements CommandLineRunner {
+
+	private static Logger logger = LoggerFactory.getLogger(JsonParserApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(JsonParserApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("STARTING JsonParserApplication via command line runner");
+	}
+
 
 }
